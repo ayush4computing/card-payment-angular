e06715125510f4c35d8e58897e89ed03195de98f
ayush <ayush.agarwal6530@gmail.com>
2023-09-22 15:47:42 +0530

    q1

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -10,30 +10,22 @@ export class TaskService {
 
   constructor() {}
 
-  getTasks(): Task[] {
-    return this.tasks;
+  getTasks(): any {
+    
   }
 
   addTask(task: Task): void {
-    task.id = this.taskIdCounter++;
-    this.tasks.push(task);
+
   }
 
-  getTaskById(id: number): Task | undefined {
-    return this.tasks.find((task) => task.id === id);
+  getTaskById(id: number): any{
   }
 
   updateTask(updatedTask: Task): void {
-    const index = this.tasks.findIndex((task) => task.id === updatedTask.id);
-    if (index !== -1) {
-      this.tasks[index] = updatedTask;
-    }
+
   }
 
   deleteTask(id: number): void {
-    const index = this.tasks.findIndex((task) => task.id === id);
-    if (index !== -1) {
-      this.tasks.splice(index, 1);
-    }
+
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -16,39 +16,16 @@ export class TasksComponent implements OnInit {
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    this.sortOrder = 'title';
-    this.filterStatus = 'all';
-    this.tasks = this.taskService.getTasks();
-    this.filteredTasks = this.tasks; // Initialize filteredTasks with all tasks
-  }
+    }
 
   sortTasks(): void {
-    this.sortFilteredTasks();
   }
 
   filterTasksByStatus(): void {
-    if (this.filterStatus === 'all') {
-      // Show all tasks
-      this.filteredTasks = this.tasks;
-    } else {
-      // Filter by status
-      this.filteredTasks = this.tasks.filter((task) => {
-        return (this.filterStatus === 'pending' && !task.done) || (this.filterStatus === 'done' && task.done);
-      });
-    }
 
-    // Sort the filtered tasks based on the selected sortOrder
-    this.sortFilteredTasks();
   }
 
   private sortFilteredTasks(): void {
-    this.filteredTasks.sort((a, b) => {
-      if (this.sortOrder === 'title') {
-        return a.title.localeCompare(b.title);
-      } else if (this.sortOrder === 'done') {
-        return a.done === b.done ? 0 : a.done ? 1 : -1;
-      }
-      return 0;
-    });
+
   }
 }
